64b6380b1979ccb6af4576f973805a2328fc80e2
DIFU-N <chukwudifunsofor@gmail.com>
2023-01-09 13:58:48 +0100

    Initial Moon Hopping

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState, useEffect } from "react";
+import axios from "axios";
+import {Routes, Route} from "react-router-dom";
+import Coins from "./components/Coins";
+
 
 function App() {
+  const [coin, setCoin] = useState([]);
+
+  const options = {
+    method: 'GET',
+    url: 'https://coinranking1.p.rapidapi.com/coins',
+    params: {
+      referenceCurrencyUuid: 'yhjMzLPhuIDl',
+      timePeriod: '24h',
+      'tiers[0]': '1',
+      orderBy: 'marketCap',
+      orderDirection: 'desc',
+      limit: '50',
+      offset: '0'
+    },
+    headers: {
+      'X-RapidAPI-Key': '86f833605emsh83e174d75b9d92ep101ef3jsn07194cd8227d',
+      'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+    }
+  };
+
+  useEffect(() => {
+    axios.request(options).then((response) => {
+      setCoin(response.data.data.coins);
+      console.log(response.data.data.coins);
+    }).catch((error) => {
+      console.error(error);
+    });
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Coins mis={coin}/>} />
+    </Routes>
   );
 }
 
---
+++ src/components/Coins.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Coins = (props) => {
+  return (
+    <div className='m-auto max-w-[1140px]'>
+      loko
+
+      {props.mis.map(loa => {
+        return (
+          <Link >
+            
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Coins;
